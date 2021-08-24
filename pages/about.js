b10f523e857fafb937661eafd1d970ec20579ac8
Maksymilian <zalmanix@gmail.gom>
2021-08-24 08:30:12 +0200

    apos fix

--- pages/about.js
+++ pages/about.js
@@ -15,15 +15,15 @@ export default function Home() {
         </div>
         <div className="bg-white mb-4">
           <p className="text-4 tracking-tight mb-4 text-black dark:text-white text-justify">
-            I'm full stack programmer currently working from home, I like to design web pages using Next.js, Angular or Laravel, and developing small indie games in my spare time.
-            I'm enjoying challenges and difficul projects. I once tried 3D moddeling in blender, it didn't go that bad but my passion lies in programming. Besides sitting 12h a day before my PC i enjoy cooking mealf for 
+            I&apos;m full stack programmer currently working from home, I like to design web pages using Next.js, Angular or Laravel, and developing small indie games in my spare time.
+            I&apos;m enjoying challenges and difficul projects. I once tried 3D moddeling in blender, it didn&apos;t go that bad but my passion lies in programming. Besides sitting 12h a day before my PC i enjoy cooking mealf for 
             my entire family. In my free time I read literature, specifficly sci-fi and fition. I have a golden retriever puppy named "Alba" witch i take on walks regullary to
             nearby forest. 
           </p>
         </div>
 
         <div className="bg-white flex justify-center place-items-center w-full pb-8">
-            <svg aria-hidden="true" class="h-8 svg-inline--fa fa-envelope fa-w-16" data-icon="envelope" data-prefix="fas" viewBox="0 0 512 512"><path fill="currentColor" d="M502 191c4-3 10 0 10 5v204c0 27-21 48-48 48H48c-26 0-48-21-48-48V196c0-5 6-8 10-5l154 114c21 15 56 47 92 47s72-33 92-47l154-114zM256 320c23 0 57-29 73-41 133-97 143-105 174-129 6-5 9-12 9-19v-19c0-26-22-48-48-48H48C22 64 0 86 0 112v19c0 7 3 14 9 19 31 24 41 32 174 129 16 12 50 41 73 41z"/></svg>
+            <svg aria-hidden="true" className="h-8 svg-inline--fa fa-envelope fa-w-16" data-icon="envelope" data-prefix="fas" viewBox="0 0 512 512"><path fill="currentColor" d="M502 191c4-3 10 0 10 5v204c0 27-21 48-48 48H48c-26 0-48-21-48-48V196c0-5 6-8 10-5l154 114c21 15 56 47 92 47s72-33 92-47l154-114zM256 320c23 0 57-29 73-41 133-97 143-105 174-129 6-5 9-12 9-19v-19c0-26-22-48-48-48H48C22 64 0 86 0 112v19c0 7 3 14 9 19 31 24 41 32 174 129 16 12 50 41 73 41z"/></svg>
             <h1 className="text-4 p-3 text-black dark:text-white text-justify">
                 <a href="mailto:zalmanix.work@gmail.com">zalmanix.work@gmail.com</a>
             </h1>
--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
             Hello there! I'm Maksymilian Skórnóg
           </h1>
         </div>
-        <div class="center" className="flex flex-row justify-center place-items-center max-w-2xl mx-auto mb-16">
+        <div className="center flex flex-row justify-center place-items-center max-w-2xl mx-auto mb-16">
           <div className="flex flex-col justify-center place-items-center">
             <h1 className="text-1xl md:text-4xl tracking-tight mb-4 text-black dark:text-white text-justify">
               A web developer, game designer and Godot engine enthusiast.
@@ -37,19 +37,19 @@ export default function Home() {
         <hr className="mt-4 w-full border-1 border-gray-200 dark:border-gray-800 mb-8" />
         <div className="mb-8">
           <h1 className="pt-8 text-1xl md:text-4xl tracking-tight mb-4 text-black dark:text-white text-justify">
-            I'm Open To Work, contact me down below
+            I&apos;m Open To Work, contact me down below
           </h1>
               <div className="bg-white"> 
                 <p className="text-justify">
-                Currently I'm working in SEARGIN Sp. z o.o. as a full stack developer using Laravel and Angular frameworks. Previously i worked in ProKomp Bochnia as It technician.
-                I'm proficient in using JavaScript, Python, PHP, CSS and HTML. My favourite framework is NEXT.js and it's ease of making web designes. But if you are in need of 
+                Currently I&apos;m working in SEARGIN Sp. z o.o. as a full stack developer using Laravel and Angular frameworks. Previously i worked in ProKomp Bochnia as It technician.
+                I&apos;m proficient in using JavaScript, Python, PHP, CSS and HTML. My favourite framework is NEXT.js and it&apos;s ease of making web designes. But if you are in need of 
                 other framework specialist I can learn them realy fast and with enthusiasm.
                 </p>
               </div>
         </div>
         <hr className="w-full border-1 border-gray-200 dark:border-gray-800 mb-8" />
         <div className="bg-white flex justify-center place-items-center w-full pb-8">
-          <svg aria-hidden="true" class="h-7 svg-inline--fa fa-envelope fa-w-16" data-icon="envelope" data-prefix="fas" viewBox="0 0 512 512"><path fill="currentColor" d="M502 191c4-3 10 0 10 5v204c0 27-21 48-48 48H48c-26 0-48-21-48-48V196c0-5 6-8 10-5l154 114c21 15 56 47 92 47s72-33 92-47l154-114zM256 320c23 0 57-29 73-41 133-97 143-105 174-129 6-5 9-12 9-19v-19c0-26-22-48-48-48H48C22 64 0 86 0 112v19c0 7 3 14 9 19 31 24 41 32 174 129 16 12 50 41 73 41z"/></svg>
+          <svg aria-hidden="true" className="h-7 svg-inline--fa fa-envelope fa-w-16" data-icon="envelope" data-prefix="fas" viewBox="0 0 512 512"><path fill="currentColor" d="M502 191c4-3 10 0 10 5v204c0 27-21 48-48 48H48c-26 0-48-21-48-48V196c0-5 6-8 10-5l154 114c21 15 56 47 92 47s72-33 92-47l154-114zM256 320c23 0 57-29 73-41 133-97 143-105 174-129 6-5 9-12 9-19v-19c0-26-22-48-48-48H48C22 64 0 86 0 112v19c0 7 3 14 9 19 31 24 41 32 174 129 16 12 50 41 73 41z"/></svg>
           <h1 className="text-4 p-3 text-black dark:text-white text-justify">
             <a href="mailto:zalmanix.work@gmail.com">zalmanix.work@gmail.com</a>
           </h1>
