37f8e6d811a7056756db2fdb71d5fd72ab756ec0
zalmanix <65413567+zalmanix@users.noreply.github.com>
2021-09-06 10:32:25 +0200

    Update caveban.js

--- pages/caveban.js
+++ pages/caveban.js
@@ -6,7 +6,7 @@ import Footer from '../components/footer';
 export default function Home() {
   return (
     <Container>
-      <div className="items-center flex flex-col justify-center items-start max-w-2xl mx-auto mb-16">
+      <div className="items-center flex flex-col justify-center max-w-2xl mx-auto mb-16">
         <div className="mb-8 shadow-md border-opacity-50 rounded-md p-6 bg-white dark:bg-black">
           <h1 className="text-center border-l-8 border-transparent font-bold text-2xl md:text-4xl tracking-tight mb-4 text-black dark:text-white">
             Caveban
