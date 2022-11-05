3e98d60cb92cc8a1fa55440041cf5635fb7ee0b7
zalmanix <65413567+zalmanix@users.noreply.github.com>
2022-11-05 09:56:10 +0100

    tailwind config

--- pages/index.tsx
+++ pages/index.tsx
@@ -54,18 +54,9 @@ export default function Home() {
         </div>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      {/* <footer className={styles.footer}>
+        TODO: FOOTER
+      </footer> */}
     </div>
   )
 }
